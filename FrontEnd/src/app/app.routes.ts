8809b65ff39a5af48e35f9cda50698819b4b3b78
NachoL24 <ignaciolances24@gmail.com>
2024-08-25 17:02:30 -0300

    added routes

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './src/components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: '**', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
