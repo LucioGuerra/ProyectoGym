e0bf435f198d13e623a25b7ebb42cdbb497b9a7a
NachoL24 <ignaciolances24@gmail.com>
2024-08-27 15:23:07 -0300

    updated routes

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './src/components/home-page/home-page.component';
+import { ShifAdminScreenComponent } from './src/layout/shif-admin-screen/shif-admin-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'admin', component: ShifAdminScreenComponent },
+];
