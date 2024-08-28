b05404125e24c8d4357eaf4a4d0e836b498b8d39
NachoL24 <ignaciolances24@gmail.com>
2024-08-28 17:20:55 -0300

    added responsive elements

--- FrontEnd/src/app/src/layout/shif-admin-screen/shif-admin-screen.component.ts
+++ FrontEnd/src/app/src/layout/shif-admin-screen/shif-admin-screen.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, model, OnInit, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '@auth0/auth0-angular';
@@ -7,15 +7,27 @@ import { ToolbarComponent } from '../../components/index';
 import { DOCUMENT } from '@angular/common';
 import { DrawerComponent } from "../../components/drawer/drawer.component";
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerInputEvent, MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDividerModule} from '@angular/material/divider';
 @Component({
   selector: 'app-shif-admin-screen',
   standalone: true,
-  imports: [ToolbarComponent, MatButtonModule, MatIconModule, MatCardModule, DrawerComponent],
+  imports: [MatDividerModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,ToolbarComponent, MatButtonModule, MatIconModule, MatCardModule, DrawerComponent],
   templateUrl: './shif-admin-screen.component.html',
+  providers: [provideNativeDateAdapter()],
   styleUrl: './shif-admin-screen.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ShifAdminScreenComponent implements OnInit {
+  events = signal<string[]>([]);
+
+  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    this.events.update(events => [...events, `${type}: ${event.value}`]);
+  }
+
 
   constructor(@Inject(DOCUMENT) public document: Document, private auth: AuthService) {
     this.auth.isAuthenticated$.subscribe(isAuthenticated => {
