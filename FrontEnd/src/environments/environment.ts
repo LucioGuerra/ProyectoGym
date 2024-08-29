937f3283fa5841e7278e2c34c92ffd2a5d6d5168
LucioAh2312 <lucioguerra2001@gmail.com>
2024-08-29 19:11:14 -0300

    Change to Java

---
+++ FrontEnd/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    auth0: {
+      domain: 'dev-22vijuiwzcze21d0.us.auth0.com',
+      clientId: 'FhWYPBfGNe4ZfqSsLC21qWwtn6l4AAb3'
+    }
+  };
