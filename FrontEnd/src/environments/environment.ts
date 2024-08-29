c2c71dfadd889047d7ab309122c271e027926277
LucioAh2312 <lucioguerra2001@gmail.com>
2024-08-29 19:16:45 -0300

    Change to java

--- FrontEnd/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    auth0: {
-      domain: 'dev-22vijuiwzcze21d0.us.auth0.com',
-      clientId: 'FhWYPBfGNe4ZfqSsLC21qWwtn6l4AAb3'
-    }
-  };
