51b7ff98cef4a3c0a0106e4a7a171b979721df71
LucioAh2312 <lucioguerra2001@gmail.com>
2024-09-05 10:34:29 -0300

    Creacion de SecurityConfig

--- BackEnd/criminal/src/main/Java/com/desarrollo/criminal/configuration/SecurityConfig.java
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/configuration/SecurityConfig.java
@@ -1,14 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.desarrollo.criminal.configuration;
 
-/**
- *
- * @author lucio
- */
-public class Class {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        /* authorizeRequests(authorizeRequests -> authorizeRequests
+                .antMatchers("/public/**").permitAll()  // Permitir acceso público
+                .antMatchers("/clientes/**").hasAuthority("ROLE_CLIENTE") // Requiere el rol de cliente
+                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")  // Requiere el rol de admin
+                .anyRequest().authenticated()  // Requerir autenticación para todas las demás rutas
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt());  // Habilitar la validación de JWT*/
+        return http.build();
+    }
 
 }
