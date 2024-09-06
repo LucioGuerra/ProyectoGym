f3fc193ec3b649ea0985757f6ed2ba6b2fe892d8
LucioAh2312 <lucioguerra2001@gmail.com>
2024-09-06 10:14:03 -0300

    PCambio en la configuracion de filterChain, se configuro para que el estado del inicio de sesion sea nulo y se deshabilito la proteccion contra ataques csrf

--- BackEnd/criminal/src/main/Java/com/desarrollo/criminal/configuration/SecurityConfig.java
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/configuration/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -19,15 +21,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        return http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/api/public").permitAll()
-                        .requestMatchers("/api/private").authenticated()
-                        .requestMatchers("/api/admin").hasAuthority("ADMIN")
-                )
+        return http.csrf(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests((authorize) -> authorize
+                            .requestMatchers("/api/public").permitAll()
+                            .requestMatchers("/api/private").authenticated()
+                            .requestMatchers("/api/admin").hasAuthority("ADMIN")
+                        )
                 .cors(withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(withDefaults())
-                )
+                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 }
