b6de7754bf361ae940dce259cd8c1dac2ad8b118
LucioAh2312 <lucioguerra2001@gmail.com>
2024-09-06 09:08:50 -0300

    Implementacion de auth0 en el backend

--- BackEnd/criminal/src/main/Java/com/desarrollo/criminal/configuration/SecurityConfig.java
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/configuration/SecurityConfig.java
@@ -1,23 +1,33 @@
 package com.desarrollo.criminal.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        /* authorizeRequests(authorizeRequests -> authorizeRequests
-                .antMatchers("/public/**").permitAll()  // Permitir acceso público
-                .antMatchers("/clientes/**").hasAuthority("ROLE_CLIENTE") // Requiere el rol de cliente
-                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")  // Requiere el rol de admin
-                .anyRequest().authenticated()  // Requerir autenticación para todas las demás rutas
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt());  // Habilitar la validación de JWT*/
-        return http.build();
-    }
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        return http.authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/public").permitAll()
+                        .requestMatchers("/api/private").authenticated()
+                        .requestMatchers("/api/admin").hasAuthority("ADMIN")
+                )
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                )
+                .build();
+    }
 }
