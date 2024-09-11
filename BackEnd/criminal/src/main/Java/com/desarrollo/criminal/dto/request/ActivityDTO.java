578cdfe501479e493e381e44ebafc7cb02af0709
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2024-09-11 16:38:37 -0300

    Creación de DTO del activity

---
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/dto/request/ActivityDTO.java
@@ -0,0 +1,14 @@
+package com.desarrollo.criminal.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ActivityDTO {
+    private String name;
+    private String description;
+    // private List<ActivityRoutineDTO> routines;
+}
