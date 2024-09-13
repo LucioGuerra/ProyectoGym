b8dda20f32b2e129f71c62e7927c5037aee5546d
NachoL24 <ignaciolances24@gmail.com>
2024-09-13 13:07:06 -0300

    Added validators to DTO

--- BackEnd/criminal/src/main/Java/com/desarrollo/criminal/dto/request/AppointmentDTO.java
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/dto/request/AppointmentDTO.java
@@ -2,23 +2,35 @@ package com.desarrollo.criminal.dto.request;
 
 import com.desarrollo.criminal.entity.Activity;
 import com.desarrollo.criminal.entity.user.User;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Getter
 @Setter
 public class AppointmentDTO {
-
+    @NotNull(message = "The date is required")
+    @FutureOrPresent(message = "The date must be in the present or future")
+    @Valid
     private LocalDate date;
+
+    @NotNull(message = "The start time is required")
+    @Valid
     private LocalTime startTime;
+
+    @NotNull(message = "The end time is required")
+    @Valid
     private LocalTime endTime;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+
+    @NotNull(message = "The activity is required")
+    @Valid
     private Activity activity;
+
     private User instructor;
 
     private AppointmentDTO() {}
