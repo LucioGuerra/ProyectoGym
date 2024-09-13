440ce1760d4633bec7d4aa46e4e0e792ca8a523d
MartinEliseche <tinchoeliseche@gmail.com>
2024-09-13 17:24:14 -0300

    Framework/backend/scrum2 crear aquitectura usuario (#6)
    * Se sacó UserState, editando a User como estaba originalmente, y se agregó UserService (views)
    
    * Se agregaron dos funciones más al UserService y se corrigieron las funciones CRUD de UserController
    
    * Se trasladó la logica de negocio así también como las Http Response al lado del UserService, dejando más sencilla la funcionalidad del Controller
    
    * Se hicieron las últimas correcciones antes del merge
    
    * Preparando merge
    
    ---------
    
    Co-authored-by: LucioAh2312 <lucioguerra2001@gmail.com>

---
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/service/UserService.java
@@ -0,0 +1,81 @@
+package com.desarrollo.criminal.service;
+
+import com.desarrollo.criminal.entity.user.User;
+import com.desarrollo.criminal.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> allUsers = userRepository.findAll();  
+
+        if (allUsers.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(allUsers);
+        }
+    }
+
+    public ResponseEntity<User> getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(user.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    public ResponseEntity<User> createUser(User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    public ResponseEntity<User> updateUser(Long id, User userDetail) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+
+            User user = optionalUser.get();
+
+            user.setFirstName(userDetail.getFirstName());
+            user.setLastName(userDetail.getLastName());
+            user.setEmail(userDetail.getEmail());
+            user.setDni(userDetail.getDni());
+            user.setRole(userDetail.getRole());
+            user.setPhone(userDetail.getPhone());
+            user.setCreditExpiration(userDetail.getCreditExpiration());
+
+            User updatedUser = userRepository.save(user);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
+
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    
+    private User saveUser(User user) {
+        return userRepository.save(user);
+    }
+ 
+}
+
+
+
+    
+    
+
+
