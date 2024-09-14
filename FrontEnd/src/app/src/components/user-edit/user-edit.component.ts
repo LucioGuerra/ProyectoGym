46f95ea9cd5416e29fd64e37d574e73c232760f2
MartinEliseche <tinchoeliseche@gmail.com>
2024-09-13 22:03:48 -0300

    Se transformo el html a formulario, se ajustaron los estilos y se creo la funcion de comparar roles en ts para poder localiazar el rol actual del usuario

--- FrontEnd/src/app/src/components/user-edit/user-edit.component.ts
+++ FrontEnd/src/app/src/components/user-edit/user-edit.component.ts
@@ -1,38 +1,45 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
-import { User } from '../models/user.models';
+import { User, Role } from '../models/user.models';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { Router } from '@angular/router';
-import { MatFormField } from '@angular/material/form-field';
-
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatCard } from '@angular/material/card';
+import { DrawerComponent } from '../drawer/drawer.component';
+import { CommonModule } from '@angular/common';
+import { MatOption } from '@angular/material/core';
 
 @Component({
   selector: 'app-user-edit',
   standalone: true,
-  imports: [ToolbarComponent, MatIconModule, FormsModule, MatIconButton, MatButtonModule, MatFormField],
+  imports: [ToolbarComponent, MatIconModule, FormsModule, MatIconButton, MatButtonModule, MatFormField, MatCard, DrawerComponent, CommonModule, MatLabel, MatOption, ReactiveFormsModule],
   templateUrl: './user-edit.component.html',
-  styleUrl: './user-edit.component.scss',
+  styleUrl: './user-edit.component.scss', 
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class UserEditComponent implements OnInit {
-  user: User = {
-    id: 123,
-    firstName: 'Martin',
-    lastName: 'Eliseche',
-    email: 'tinchoeliseche@gmail.com',
-    role: { id: 1, name: 'Admin' },
-    phone: 2262369000,
-    password: '1234',
-    creditExpiration: new Date(),
-  };
+  user: User;
+  roles: { id: number; name: string }[] = [
+    { id: 1, name: 'User' },
+    { id: 2, name: 'Admin' }
+  ];
 
-  userImage: string = "../../../../assets/picture.jpg";
+  userImage: string | any;
 
   constructor(public auth: AuthService, private router: Router){
+    this.user = {
+      id: 123,
+      firstName: 'Martin',
+      lastName: 'Eliseche',
+      email: 'tinchoeliseche@gmail.com',
+      role: { id: 2, name: 'Admin' },
+      phone: 2262369000,
+      password: '1234',
+      creditExpiration: new Date(),
+    };
   }
 
   ngOnInit(): void {
@@ -40,9 +47,12 @@ export class UserEditComponent implements OnInit {
       this.userImage = profile.picture;
     });
   }
-  
 
-  onPasswordChange(): void {
+  compareRoles(role1: any, role2: any): boolean {
+    return role1 && role2 ? role1.id === role2.id : role1 === role2;
+  }
+
+  PasswordChange(): void {
     console.log("Password's change button clicked");
   }
 
@@ -59,10 +69,6 @@ export class UserEditComponent implements OnInit {
   }
 
   saveChanges() {
-    alert('changes were saved.');
+    alert('Changes were saved.');
   }
-
-
 }
-
-
