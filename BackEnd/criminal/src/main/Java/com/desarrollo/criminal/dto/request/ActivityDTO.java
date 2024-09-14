bd7808bc46aa3c94d3fd0ed7314b5690fefa86c6
Ariana Wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2024-09-13 22:13:56 -0300

    Framework: Crear Actividad | SCRUM-89 (#7)
    * Creación de DTO del activity
    
    * Modificacion de ServiceActivity, ControllerActivity y ActivityDTO
    
    * correcciones
    
    * Arreglando bugs
    
    * ActivityDTO validations added
    
    * Arreglando inconsistencias.

---
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/dto/request/ActivityDTO.java
@@ -0,0 +1,24 @@
+package com.desarrollo.criminal.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ActivityDTO {
+
+    @NotBlank(message = "Activity name cannot be empty.")
+    @Size(min = 1, max = 20, message = "Activity name must be between 1 and 20 characters.")
+    private String name;
+
+    @Size(max = 100, message = "Activity description must have a maximum of 100 characters.")
+    private String description;
+
+    @NotNull(message = "Price cannot be null.")
+    @Digits(integer = 6, fraction = 2, message = "Price must have a maximum of 6 integer digits and 2 decimal digits.")
+    @PositiveOrZero(message = "Price cannot be negative.")
+    private float price;
+}
