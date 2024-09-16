cec82917b06349766abb379b44d98582ba43308f
MartinEliseche <tinchoeliseche@gmail.com>
2024-09-15 21:46:34 -0300

    Se reestructuró el html para que este en formato de mat-card, tambien se realizaron validaciones en los respectivos campos a modificar. Queda pendiente solucionar un inconveniente con la toolbar y el boton para setear el rol del usuario.

--- FrontEnd/src/app/src/components/models/user.models.ts
+++ FrontEnd/src/app/src/components/models/user.models.ts
@@ -6,6 +6,7 @@ export interface User {
     role: Role;
     phone: number;
     password: string;
+    dni: number;
     //photo: blob;
     creditExpiration: Date;
 }
--- FrontEnd/src/app/src/components/user-edit/user-edit.component.ts
+++ FrontEnd/src/app/src/components/user-edit/user-edit.component.ts
@@ -7,73 +7,106 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatCard } from '@angular/material/card';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
 import { DrawerComponent } from '../drawer/drawer.component';
 import { CommonModule } from '@angular/common';
 import { MatOption } from '@angular/material/core';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
 
 @Component({
   selector: 'app-user-edit',
   standalone: true,
-  imports: [ToolbarComponent, MatIconModule, FormsModule, MatIconButton, MatButtonModule, MatFormField, MatCard, DrawerComponent, CommonModule, MatLabel, MatOption, ReactiveFormsModule],
+
+  imports: [ToolbarComponent, MatIconModule, FormsModule, MatIconButton, MatButtonModule, MatFormField, MatCard, DrawerComponent, CommonModule, MatLabel, MatOption, ReactiveFormsModule, MatSelect, MatDividerModule, MatCardHeader,MatCardContent, MatButtonToggleModule, MatSelectModule],
+  
   templateUrl: './user-edit.component.html',
   styleUrl: './user-edit.component.scss', 
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class UserEditComponent implements OnInit {
-  user: User;
+  user: User;  
+  userImage: string | undefined;  
   roles: { id: number; name: string }[] = [
-    { id: 1, name: 'User' },
-    { id: 2, name: 'Admin' }
+    { id: 1, name: 'Admin' },
+    { id: 2, name: 'User' }
   ];
 
-  userImage: string | any;
-
-  constructor(public auth: AuthService, private router: Router){
+  constructor(public auth: AuthService, private router: Router) {
+    
     this.user = {
       id: 123,
       firstName: 'Martin',
       lastName: 'Eliseche',
       email: 'tinchoeliseche@gmail.com',
-      role: { id: 2, name: 'Admin' },
+      role: { id: 1, name: 'Admin' },
       phone: 2262369000,
       password: '1234',
       creditExpiration: new Date(),
+      dni: 12093847,
     };
   }
 
-  optionrole() {
-    if (this.user.role.name == "Admin")
-      return "User"
-    else
-      return "Admin"
-  }
-
-
-
   ngOnInit(): void {
     this.auth.user$.subscribe((profile: any) => {
-      this.userImage = profile.picture;
+      this.userImage = profile?.picture;
     });
   }
 
-  PasswordChange(): void {
-    console.log("Password's change button clicked");
-  }
-
-  logout() {
-    this.router.navigate(['/login']);
-  }
-
+  
   homePage() {
     this.router.navigate(['/home']);
   }
 
-  email() {
-    alert('Warning: The value of this section cannot be modified.');
+ 
+  PasswordChange(): void {
+    alert('Changing password!');
+    this.router.navigate(['/change-password']);
   }
 
+  
+  email() {
+    alert('Warning: The email cannot be modified.');
+  }
+
+ 
   saveChanges() {
-    alert('Changes were saved.');
+
+    if (!this.user.firstName || this.user.firstName.trim() === '') {
+      alert('Name cannot be empty');
+      return;
+    }
+
+    if (!this.user.lastName || this.user.lastName.trim() === '') {
+      alert('Surname cannot be empty');
+      return;
+    }
+
+    if (!this.user.dni) {
+      alert('DNI cannot be empty');
+      return;
+    }
+
+    if (isNaN(this.user.dni) || this.user.dni.toString().length !== 8) {
+      alert('DNI must be an 8 digit number');
+      return;
+    }
+
+    if (!this.user.phone) {
+      alert('Mobile cannot be empty');
+      return;
+    }
+
+    if (isNaN(this.user.phone)) {
+      alert('Mobile must be a number');
+      return;
+    }
+
+    alert('Changes were successfully saved.');
+
   }
 }
+
