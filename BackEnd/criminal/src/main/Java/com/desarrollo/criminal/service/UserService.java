2053a8b346fcef7e7475f6a5c7b94a0262a0d1ba
NachoL24 <ignaciolances24@gmail.com>
2024-09-16 10:52:33 -0300

    update: used entity not found exception

--- BackEnd/criminal/src/main/Java/com/desarrollo/criminal/service/UserService.java
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/service/UserService.java
@@ -6,6 +6,7 @@ import com.desarrollo.criminal.entity.tracking.DateWeight;
 import com.desarrollo.criminal.entity.tracking.Tracking;
 import com.desarrollo.criminal.entity.user.User;
 import com.desarrollo.criminal.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.http.HttpStatus;
@@ -36,8 +37,7 @@ public class UserService {
     }
 
     public User getUserById(Long id) {
-        return userRepository.findById(id).orElseThrow( () -> new RuntimeException("User not found"));
-
+        return userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
     public ResponseEntity<User> createUser(User user) {
