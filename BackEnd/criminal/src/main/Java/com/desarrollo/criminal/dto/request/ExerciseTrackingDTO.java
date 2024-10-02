81a3e15fc38f6bf4390372efd4289f64580bbdcd
NachoL24 <ignaciolances24@gmail.com>
2024-10-02 09:13:32 -0300

    added validations to request dto

--- BackEnd/criminal/src/main/Java/com/desarrollo/criminal/dto/request/ExerciseTrackingDTO.java
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/dto/request/ExerciseTrackingDTO.java
@@ -1,15 +1,26 @@
 package com.desarrollo.criminal.dto.request;
 
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 @Getter
 @Setter
 @AllArgsConstructor
 public class ExerciseTrackingDTO {
+
+    @NotNull(message = "Weight is required")
+    @PositiveOrZero(message = "Weight must be positive")
     private Double weight;
+
+    @NotNull(message = "Date is required")
+    @PastOrPresent(message = "Date can not be in the future")
     private Date date;
+
+    @NotNull(message = "ExerciseID is required")
     private Long ExerciseID;
 }
