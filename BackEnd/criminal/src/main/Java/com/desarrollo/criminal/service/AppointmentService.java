ab57d5e42c0b01074fdbb420391cda60b53c25b6
NachoL24 <ignaciolances24@gmail.com>
2024-10-02 10:51:26 -0300

    changed logic on create for not using nulls

--- BackEnd/criminal/src/main/Java/com/desarrollo/criminal/service/AppointmentService.java
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/service/AppointmentService.java
@@ -66,26 +66,37 @@ public class AppointmentService {
             throw new CriminalCrossException("INVALID_TIME_RANGE", "The start time must be before the end time");
         }
 
-        if (appointmentDTO.getEndDate() == null || appointmentDTO.getEndDate().isAfter(appointmentDTO.getDate())){
+        if (appointmentDTO.getEndDate() != null && !appointmentDTO.getEndDate().isAfter(appointmentDTO.getDate())){
             throw new CriminalCrossException("INVALID_DATE_RANGE", "The end date must be after the start date");
         }
 
-        //el modelmapper me da error
-        // Appointment appointment = modelMapper.map(appointmentDTO, Appointment.class);
         Activity activity = activityService.getActivityById(appointmentDTO.getActivityID());
 
         Optional<User> instructorOptional = Optional.ofNullable(appointmentDTO.getInstructorID())
                 .map(userService::getUserById);
 
-        if (appointmentDTO.getEndDate() != null && !appointmentDTO.getAppointmentWeekDays().isEmpty()) {
-            Long recurrenceId = null; //esto se usa para guardar el id de la primera cita que se crea, no se guarda como null
+        Appointment appointment = new Appointment();
+        appointment.setDate(appointmentDTO.getDate());
+        appointment.setStartTime(appointmentDTO.getStartTime());
+        appointment.setEndTime(appointmentDTO.getEndTime());
+        appointment.setActivity(activity);
+        appointment.setMax_capacity(appointmentDTO.getMax_capacity());
+        if (instructorOptional.isPresent()) {
+            appointment.setInstructor(instructorOptional.get());
+        }
+        appointment = appointmentRepository.save(appointment);
+        Long recurrenceId = appointment.getId();
+        appointment.setRecurrenceId(recurrenceId);
+        appointmentRepository.save(appointment);
 
-            LocalDate appointmentDate = appointmentDTO.getDate();
+        if (appointmentDTO.getEndDate() != null && !appointmentDTO.getAppointmentWeekDays().isEmpty()) {
+
+            LocalDate appointmentDate = appointmentDTO.getDate().plusDays(1);
 
             while (appointmentDate.isBefore(appointmentDTO.getEndDate())) {
                 if (appointmentDTO.getAppointmentWeekDays().contains(appointmentDate.getDayOfWeek())) {
 
-                    Appointment appointment = new Appointment();
+                    appointment = new Appointment();
                     appointment.setDate(appointmentDate);
                     appointment.setStartTime(appointmentDTO.getStartTime());
                     appointment.setEndTime(appointmentDTO.getEndTime());
@@ -94,27 +105,12 @@ public class AppointmentService {
                         appointment.setInstructor(instructorOptional.get());
                     }
                     appointment.setMax_capacity(appointmentDTO.getMax_capacity());
+                    appointment.setRecurrenceId(recurrenceId);
+                    appointmentRepository.save(appointment);
 
-                    if (recurrenceId == null) {
-                        appointment = appointmentRepository.save(appointment);
-                        recurrenceId = appointment.getId(); // Get the generated ID
-                        appointment.setRecurrenceId(recurrenceId); // Set recurrenceId to its own ID
-                        appointmentRepository.save(appointment); // Update the appointment with recurrenceId
-                    } else {
-                        appointment.setRecurrenceId(recurrenceId); // Set recurrenceId for subsequent appointments
-                        appointmentRepository.save(appointment);
-                    }
                 }
                 appointmentDate = appointmentDate.plusDays(1);
             }
-        } else {
-            Appointment appointment = new Appointment();
-            appointment.setDate(appointmentDTO.getDate());
-            appointment.setStartTime(appointmentDTO.getStartTime());
-            appointment.setEndTime(appointmentDTO.getEndTime());
-            appointment.setActivity(activity);
-            appointment.setMax_capacity(appointmentDTO.getMax_capacity());
-            appointmentRepository.save(appointment);
         }
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
