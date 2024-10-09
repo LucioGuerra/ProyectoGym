0eb457c0a40795aa3de22398e006143eed1ea061
NachoL24 <ignaciolances24@gmail.com>
2024-10-09 19:22:13 -0300

    created error dialog

---
+++ FrontEnd/src/app/src/components/dialog/error-dialog/error-dialog.component.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle, } from '@angular/material/dialog';
+import {MatButtonModule} from "@angular/material/button";
+import {Router} from "@angular/router";
+@Component({
+  selector: 'app-error-dialog',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule,
+  ],
+  template: `
+    <h1 mat-dialog-title>Error</h1>
+    <div mat-dialog-content>
+      <p>{{ data.message }}</p>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button (click)="onClose()">Close</button>
+    </div>
+  `,
+  styleUrl: './error-dialog.component.scss',
+})
+export class ErrorDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ErrorDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { message: string },
+    private router: Router,
+  ) {}
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
+}
