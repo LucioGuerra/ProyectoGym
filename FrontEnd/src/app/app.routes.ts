801c705d595cdc4a272ef54a3f1417e88fceb0c5
NachoL24 <ignaciolances24@gmail.com>
2024-10-12 16:57:59 -0300

    modified css and added routes

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,18 +1,20 @@
-import { Routes } from '@angular/router';
-import { HomePageComponent } from './src/components/home-page/home-page.component';
-import { LoginComponent } from './src/components/login/login.component';
-import { SingupComponent } from './src/components/singup/singup.component';
-import { ShifAdminScreenComponent } from './src/layout/shif-admin-screen/shif-admin-screen.component';
-import { UserEditComponent } from './src/components/user-edit/user-edit.component';
+import {Routes} from '@angular/router';
+import {HomePageComponent} from './src/components/home-page/home-page.component';
+import {LoginComponent} from './src/components/login/login.component';
+import {SingupComponent} from './src/components/singup/singup.component';
+import {ShifAdminScreenComponent} from './src/layout/shif-admin-screen/shif-admin-screen.component';
+import {UserEditComponent} from './src/components/user-edit/user-edit.component';
 import {CreateAppointmentComponent} from "./src/layout/create-appointment/create-appointment.component";
+import {ClientAgendaComponent} from "./src/layout/client-agenda/client-agenda.component";
 
 export const routes: Routes = [
-    { path: 'home', component: HomePageComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SingupComponent },
-    { path: 'admin/agenda', component: ShifAdminScreenComponent },
-    { path: 'edit', component: UserEditComponent},
-    {path: 'admin/appointment/create', component: CreateAppointmentComponent},
-    {path: 'admin/appointment/edit/:id', component: CreateAppointmentComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SingupComponent},
+  {path: 'admin/agenda', component: ShifAdminScreenComponent},
+  {path: 'edit', component: UserEditComponent},
+  {path: 'admin/appointment/create', component: CreateAppointmentComponent},
+  {path: 'admin/appointment/edit/:id', component: CreateAppointmentComponent},
+  {path: 'agenda', component: ClientAgendaComponent},
 ];
