d396abe32aa2fa5d11e7f4aab5610eb615dcb292
NachoL24 <ignaciolances24@gmail.com>
2024-10-12 16:29:18 -0300

    fixed drawer

--- FrontEnd/src/app/src/components/drawer/drawer.component.ts
+++ FrontEnd/src/app/src/components/drawer/drawer.component.ts
@@ -1,10 +1,10 @@
 import { Component, Inject } from '@angular/core';
-import { ToolbarComponent } from "../toolbar/toolbar.component";
+import { ToolbarComponent } from "../toolbar";
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { DrawerListComponent } from "./drawer-list/drawer-list.component";
 import { MatIconModule } from '@angular/material/icon';
 import { DOCUMENT } from '@angular/common';
-import { AuthService } from '../services/services/auth.service';
+import { AuthService } from '../services/services';
 import { MatButtonModule } from '@angular/material/button';
 import { UserProfileComponent } from "../user-profile/user-profile.component";
 
@@ -20,20 +20,7 @@ export class DrawerComponent {
   events: string[] = [];
   opened: boolean = true;
 
-  constructor(@Inject(DOCUMENT) public document: Document, private auth0: AuthService) {
-
-  constructor(@Inject(DOCUMENT) public document: Document, private auth: AuthService) {
-    this.auth.isAuthenticated$.subscribe(isAuthenticated => {
-      console.log('isAuthenticated:', isAuthenticated);
-      if(!isAuthenticated) {
-        this.auth.loginWithRedirect();
-      }else{
-        this.auth.user$.subscribe(user => {
-          console.log('User:', user);
-        });
-      }
-    });
-  }
+  constructor(@Inject(DOCUMENT) public document: Document, private auth0: AuthService) {}
 
   menu() {
     alert('menu');
