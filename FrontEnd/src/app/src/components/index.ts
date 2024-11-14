c781eeed8cfbe30e2e9c25b7ac38986a45d61e65
NachoL24 <ignaciolances24@gmail.com>
2024-11-14 11:32:08 -0300

    added service to barrel

--- FrontEnd/src/app/src/components/index.ts
+++ FrontEnd/src/app/src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './toolbar/toolbar.component';
 export * from './models/index';
 export * from './services/services/index';
+export * from './services/shop-list/shop-list.service';
 export * from '../../../index';
