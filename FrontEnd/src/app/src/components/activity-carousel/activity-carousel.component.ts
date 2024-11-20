a0ce45c71e81bd41fa83836b36c8bc38e52b3960
NachoL24 <ignaciolances24@gmail.com>
2024-11-20 13:21:04 -0300

    carrusel v2

--- FrontEnd/src/app/src/components/activity-carousel/activity-carousel.component.ts
+++ FrontEnd/src/app/src/components/activity-carousel/activity-carousel.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ActivityService } from './../services/services/activity.service';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Activity, Announcement } from '../models';
 import { AnnouncementService } from '..';
 import { MatIconModule } from '@angular/material/icon';
@@ -19,36 +20,57 @@ import { MatTabsModule } from '@angular/material/tabs';
   styleUrl: './activity-carousel.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ActivityCarouselComponent {
+export class ActivityCarouselComponent implements OnInit {
   
-  @Input() activities: Activity[] = [];
+  activities: Activity[] = [];
+  groupedActivities: Activity[][] = [];
   
-  selectedIndex = 0;
-  announcements: Announcement[] = [];
+  firstCarouselIndex = 0;
+  secondCarouselIndex = 0;
+  thirdCarouselIndex = 0;
 
-  constructor(private announcementService: AnnouncementService) {}
+  constructor(private activityService: ActivityService) {
+    this.loadActivities();
+  }
+
+  loadActivities() {
+    this.activityService.getActivities().subscribe(activities => {
+      this.activities = activities;
+      this.groupActivities();
+      this.secondCarouselIndex = 1;
+      this.thirdCarouselIndex = 2;
+    });
+  }
 
   ngOnInit() {
-    this.announcements = this.announcementService.getAnnouncements();
+    console.log('Actividades agrupadas:', this.groupedActivities);
+    console.log('Actividades:', this.activities);
   }
+
+  groupActivities() {
+    // Agrupar actividades en grupos de 3
+    this.groupedActivities = [];
+    for (let i = 0; i < this.activities.length; i += 3) {
+      this.groupedActivities.push(this.activities.slice(i, i + 3));
+    }
+  }
+
 
   prevSlide() {
     // Ir al slide anterior
-    if (this.selectedIndex > 0) {
-      this.selectedIndex--;
-    }
-    if (this.selectedIndex === 0) {
-      this.selectedIndex = this.announcements.length - 1;
+    if (this.firstCarouselIndex > 0) {
+      this.firstCarouselIndex--;
+      this.secondCarouselIndex--;
+      this.thirdCarouselIndex--;
     }
   }
 
   nextSlide() {
     // Ir al siguiente slide
-    if (this.selectedIndex < this.announcements.length - 1) {
-      this.selectedIndex++;
-    }
-    if (this.selectedIndex === this.announcements.length - 1) {
-      this.selectedIndex = 0;
+    if (this.thirdCarouselIndex < this.activities.length - 1) {
+      this.firstCarouselIndex++;
+      this.secondCarouselIndex++;
+      this.thirdCarouselIndex++;
     }
   }
 }
