911030df57b99ff502ccdbe7e96dde79e498937a
LucioGuerra <lucioguerra2001@gmail.com>
2024-11-29 14:24:05 -0300

    Restauracion y mejora de announcement

---
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/controller/AnnouncementController.java
@@ -0,0 +1,54 @@
+package com.desarrollo.criminal.controller;
+
+import java.util.List;
+
+import com.desarrollo.criminal.dto.response.GetAnnouncementDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.desarrollo.criminal.dto.request.AnnouncementDTO;
+import com.desarrollo.criminal.entity.Announcement;
+import com.desarrollo.criminal.service.AnnouncementService;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/public/announcements")
+public class AnnouncementController{
+
+    private final AnnouncementService announcementService;
+
+
+    @PostMapping
+    public ResponseEntity<com.desarrollo.criminal.dto.request.AnnouncementDTO> createAnnouncement(@Valid @RequestBody AnnouncementDTO announcementDTO){
+
+        return announcementService.createAnnouncement(announcementDTO);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<GetAnnouncementDTO>> getAllAnnouncement(){
+        return announcementService.getAllAnnouncement();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GetAnnouncementDTO> getAnnouncementById(@PathVariable Long id){
+        return announcementService.getAnnouncementDTOById(id);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAnnouncement(@PathVariable Long id){
+        return announcementService.deleteAnnouncement(id);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AnnouncementDTO> updateAnnouncement(@PathVariable Long id, @Valid @RequestBody AnnouncementDTO announcementDTO){
+        return announcementService.updateAnnouncement(id, announcementDTO);
+    }
+
+}
