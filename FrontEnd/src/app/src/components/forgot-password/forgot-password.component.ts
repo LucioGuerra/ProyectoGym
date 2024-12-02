d28d8f09be9e34c40b45b3be333573f153d14110
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2024-12-02 19:31:17 -0300

    Login listo, totalmente responsive y en español

--- FrontEnd/src/app/src/components/forgot-password/forgot-password.component.ts
+++ FrontEnd/src/app/src/components/forgot-password/forgot-password.component.ts
@@ -1,13 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ForgotPasswordComponent {
+  constructor(private router: Router) {
+  }
 
+  back() {
+    window.history.back()
+  }
 }
