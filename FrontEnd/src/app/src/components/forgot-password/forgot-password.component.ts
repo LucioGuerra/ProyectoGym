1b8a6b8d69d28b8d097792615a7bcea604254c4d
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2024-12-02 21:02:03 -0300

    Agregué la ruta del login

--- FrontEnd/src/app/src/components/forgot-password/forgot-password.component.ts
+++ FrontEnd/src/app/src/components/forgot-password/forgot-password.component.ts
@@ -6,6 +6,8 @@ import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {ToolbarComponent} from "../toolbar";
 import {MyErrorStateMatcher} from "../signup/signup.component";
+import {MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-forgot-password',
@@ -20,7 +22,9 @@ import {MyErrorStateMatcher} from "../signup/signup.component";
     MatFormField,
     MatInput,
     MatLabel,
-    ToolbarComponent
+    ToolbarComponent,
+    MatButton,
+    MatIcon
   ],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss',
@@ -40,4 +44,7 @@ export class ForgotPasswordComponent {
     window.history.back()
   }
 
+  login() {
+    this.router.navigate(["/login"]);
+  }
 }
