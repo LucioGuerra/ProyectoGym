b89279bbd7f004a9d96d6fa76c4a6d59d9dd7292
NachoL24 <ignaciolances24@gmail.com>
2024-12-04 12:30:55 -0300

    creacion del dialogo de confirmacion

---
+++ FrontEnd/src/app/src/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrl: './confirmation-dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ConfirmationDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { message: string },
+  ) {}
+
+  cancel(): void {
+    this.dialogRef.close(false);
+  }
+  acept(): void {
+    this.dialogRef.close(true);
+  }
+}
