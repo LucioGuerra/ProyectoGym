d6ca186c1af7fee9c98126bc7b9556e8b1e19c60
LucioGuerra <lucioguerra2001@gmail.com>
2024-12-04 20:07:39 -0300

    Añadidos manejadores de excepciones comunes

--- BackEnd/criminal/src/main/Java/com/desarrollo/criminal/handler/GlobalExceptionHandler.java
+++ BackEnd/criminal/src/main/Java/com/desarrollo/criminal/handler/GlobalExceptionHandler.java
@@ -6,16 +6,23 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(EntityNotFoundException.class)
     private ResponseEntity<ApiError> handlerEntityNotFoundException(EntityNotFoundException e){
         log.error(String.valueOf(e));
@@ -25,7 +32,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(apiError.status()).body(apiError);
     }
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(CriminalCrossException.class)
     private ResponseEntity<ApiError> handlerCriminalCrossException(CriminalCrossException e){
         log.error(String.valueOf(e));
@@ -34,4 +40,41 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(apiError.status()).body(apiError);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<ApiError> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        log.error(String.valueOf(e));
+
+        ApiError apiError = new ApiError("Validation error", e.getMessage(), BAD_REQUEST.value());
+
+        return ResponseEntity.status(apiError.status()).body(apiError);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    private ResponseEntity<ApiError> handlerHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        log.error(String.valueOf(e));
+
+        ApiError apiError = new ApiError("Malformed JSON request", e.getMessage(), BAD_REQUEST.value());
+
+        return ResponseEntity.status(BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity<ApiError> handlerException(Exception e){
+        log.error(String.valueOf(e));
+
+        ApiError apiError = new ApiError("Internal server error", e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return ResponseEntity.status(apiError.status()).body(apiError);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<ApiError> handlerDataIntegrityViolationException(DataIntegrityViolationException e){
+        log.error(String.valueOf(e));
+
+        ApiError apiError = new ApiError("Data integrity violation", e.getMessage(), BAD_REQUEST.value());
+
+        return ResponseEntity.status(BAD_REQUEST).body(apiError);
+    }
+
 }
