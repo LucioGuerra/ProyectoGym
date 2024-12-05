0851c10bbe2e2dba37cc96f564cb4e6d7634750c
LucioGuerra <lucioguerra2001@gmail.com>
2024-12-04 21:34:45 -0300

    Prueba entityNotFound

--- BackEnd/criminal/src/test/java/com/desarrollo/criminal/user/UserServiceTest.java
+++ BackEnd/criminal/src/test/java/com/desarrollo/criminal/user/UserServiceTest.java
@@ -6,16 +6,21 @@ import com.desarrollo.criminal.entity.PackageActivity;
 import com.desarrollo.criminal.entity.user.User;
 import com.desarrollo.criminal.repository.UserRepository;
 import com.desarrollo.criminal.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,6 +62,34 @@ public class UserServiceTest {
         // When
         when(userRepository.findByEmail(email)).thenReturn(Optional.of(mockUser));
         when(userRepository.findActivePackagesByUserId(mockUser.getId())).thenReturn(Optional.of(mockPackage));
+
+        ResponseEntity<List<String>> response = userService.getActivitiesUser(email);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(2, response.getBody().size());
+        assertEquals("Ejemplo", response.getBody().get(0));
+        assertEquals("Ejemplo2", response.getBody().get(1));
+    }
+
+    @Test
+    void testGetActivitiesUser_UserNotFound() {
+        // Given
+        String email = "ejemplo@ejemlo.com";
+
+        User mockUser = new User();
+        mockUser.setEmail(email);
+        mockUser.setId(1L);
+
+        // When
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        ResponseEntity<List<String>> response = userService.getActivitiesUser(email);
+
+
+        EntityNotFoundException entityNotFoundException = assertThrows(EntityNotFoundException.class, () -> {
+            throw new EntityNotFoundException("User not found with email: " + email);
+        });
 
     }
 }
